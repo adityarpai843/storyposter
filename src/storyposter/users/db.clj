c1f1c9d458982f4ab1b1ad4ffda17dc766dee3ac
Aditya R Pai <adityarpai843@gmail.com>
2023-09-03 15:14:25 +0530

    added timestamp capabilities and refactored status codes

--- src/storyposter/users/db.clj
+++ src/storyposter/users/db.clj
@@ -2,7 +2,8 @@
   (:require [buddy.core.codecs :refer :all]
             [storyposter.config :refer [db-spec]]
             [buddy.core.hash :as hash]
-            [toucan2.core :as t2]))
+            [toucan2.core :as t2])
+  (:import (java.text SimpleDateFormat)))
 
 (defn create-hashed-password
   "Generate SHA3 hash for the users password"
@@ -13,8 +14,13 @@
 (defn create-user
   "Create a user data and save it to DB"
   [user]
-  (let [db-data (-> user
+  (let [datetime-format (SimpleDateFormat. "yyyy-MM-dd HH:mm:ss")
+        current-date-time (->> (new java.util.Date)
+                               (.format datetime-format)
+                               (java.sql.Timestamp/valueOf))
+        db-data (-> user
                     (assoc :api-key (.toString (java.util.UUID/randomUUID)))
+                    (assoc :created-at current-date-time)
                     (update :password create-hashed-password))]
     (t2/insert! :conn db-spec "users" db-data)
     (:api-key db-data)))
--- src/storyposter/users/user.clj
+++ src/storyposter/users/user.clj
@@ -1,22 +1,14 @@
 (ns storyposter.users.user
   (:require [storyposter.users.db :as db]
             [schema.core :as s]
+            [storyposter.utils.status :refer [created bad-request]]
             [storyposter.users.validation :refer [UserSchema]]))
-
-(defn send-success-response
-  "Sends the API Key with status code"
-  [key]
-  {:status  201
-   :body    {:api-key key}})
-
 (defn create-user-handler
   "Handler for creating a user and returning API key"
   [request-body]
   (let [validate-schema (s/check UserSchema request-body)]
     (if (not validate-schema)
-      (->> request-body
-           db/create-user
-           send-success-response)
-      {:status  400
-       :body    {:error (str "Field" (keys validate-schema) (vals validate-schema))}})))
+      (created {:api-key (->> request-body
+                              db/create-user)})
+      (bad-request {:error (str "Field" (keys validate-schema) (vals validate-schema))}))))
 
