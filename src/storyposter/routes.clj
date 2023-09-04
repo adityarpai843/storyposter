0a48920567125eee44933f7ed92fc1b0a96832af
Aditya R Pai <adityarpai843@gmail.com>
2023-09-04 08:31:48 +0530

    modified the routes for authentication support

--- src/storyposter/routes.clj
+++ src/storyposter/routes.clj
@@ -1,18 +1,21 @@
 (ns storyposter.routes
   (:require [compojure.core :refer :all]
             [storyposter.stories.story :as s]
+            [storyposter.utils.middleware :as middleware]
             [storyposter.users.user :refer [create-user-handler]]))
 
 (def all-routes
   (routes
     (GET "/v1/stories" request (s/get-recent-stories))
     (POST "/v1/user/login" request (create-user-handler (:body request)))
-    (context "/v1/story" []
-      (POST "/" request)
-      (PATCH "/:story-id" request)
-      (PATCH "/:story-id/part/:part-id" request)
-      (DELETE "/:story-id" request)
-      (DELETE "/:story-id/part/:part-id" request))
-    (context "/v1/user" []
-      (GET "/stories" request)
-      (PUT "/story/:story-id/part/:part-id" request))))
+    (-> (context "/v1/story" []
+          (POST "/" request)
+          (PATCH "/:story-id" request)
+          (PATCH "/:story-id/part/:part-id" request)
+          (DELETE "/:story-id" request)
+          (DELETE "/:story-id/part/:part-id" request))
+        (wrap-routes middleware/user-authenticated))
+    (-> (context "/v1/user" []
+          (GET "/stories" request)
+          (PUT "/story/:story-id/part/:part-id" request))
+        (wrap-routes middleware/user-authenticated))))
